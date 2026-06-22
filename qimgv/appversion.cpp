0b942488e24773b61139da68732d0dcced5f3365
Ninja <ninja@renton.net>
2026-06-22 15:48:35 -0400

    Change version scheme to calVer

--- qimgv/appversion.cpp
+++ qimgv/appversion.cpp
@@ -1,3 +1,7 @@
 #include "appversion.h"
 
-QVersionNumber appVersion(1,0,4);
+// Versioning scheme (do-i fork, independent of upstream):
+//   major = year, minor = month, micro = sequential release that month (from 1).
+// Must stay monotonically increasing; drives the onUpdate()/changelog and
+// new-default-shortcut migration in core.cpp / actionmanager.cpp.
+QVersionNumber appVersion(2026, 6, 1);
