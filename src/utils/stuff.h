4c0d41cdbfe63dadca864203b60faa709d420266
Ninja <ninja@renton.net>
2026-07-10 13:42:56 -0400

    Remove obsolete platform probe helper

--- src/utils/stuff.h
+++ src/utils/stuff.h
@@ -2,15 +2,14 @@
 
 #include <QString>
 
+#include <string>
+
 #ifdef _WIN32
     #define StdString std::wstring
-    #define CharType wchar_t
 #else
     #define StdString std::string
-    #define CharType char
 #endif
 
 int clamp(int x, int lower, int upper);
-int probeOS();
 StdString toStdString(QString str);
 QString fromStdString(StdString str);
