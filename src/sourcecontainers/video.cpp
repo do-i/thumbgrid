140ebd36276916817685931552ba4010d6428df3
Ninja <ninja@renton.net>
2026-07-16 21:27:12 -0400

    Remove commented-out QProcess/QRegExp block and unused time.h include from video.cpp

--- src/sourcecontainers/video.cpp
+++ src/sourcecontainers/video.cpp
@@ -1,5 +1,4 @@
 #include "video.h"
-#include <time.h>
 
 Video::Video(QString _path) : Image(_path) {
     Video::load();
@@ -13,35 +12,6 @@ void Video::load() {
     if(isLoaded())
         return;
 
-    /*
-    auto mpvBinary = settings->mpvBinary();
-    if(mpvBinary.isEmpty())
-        return;
-    // Get resolution from ffmpeg (so we don't have to ask videoplayer)
-    QString command = "\"" + mpvBinary + "\"" + " -i " + "\"" + mDocInfo->filePath() + "\"";
-    QString filePathEsc = mDocInfo->filePath();
-    filePathEsc.replace("%", "%%");
-    QProcess process;
-    process.setProcessChannelMode(QProcess::MergedChannels);
-    process.start(settings->mpvBinary(), QStringList() << "-i" << filePathEsc);
-    process.waitForFinished(100);
-    QByteArray out = process.readAllStandardOutput();
-
-    QRegExp expResolution("[0-9]+x[0-9]+");
-    QRegExp expWidth("[0-9]+\\B");
-    QRegExp expHeight("\\B+[0-9]+$");
-    expResolution.indexIn(out);
-    QString res = expResolution.cap();
-    expWidth.indexIn(res);
-    expHeight.indexIn(res);
-    QString wt = expWidth.cap();
-    QString ht = expHeight.cap();
-
-    srcWidth = wt.toUInt();
-    srcHeight = ht.toUInt();
-
-    qDebug() << "zzzz" << wt << ht << filePathEsc << settings->mpvBinary();
-    */
     mLoaded = true;
 }
 
